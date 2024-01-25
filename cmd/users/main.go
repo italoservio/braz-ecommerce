1c7aba5ffa052bce52125bdf6654a1165d7a4c84
italoservio <italo.carvalho@pluto.tv>
2024-01-24 23:17:21 -0300

    feat(BRAZ-21): add project structure and first dockerfile

--- cmd/users/main.go
+++ cmd/users/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type HelloWorld struct {
+	Foo   string `json:"foo"`
+	Fizz  string `json:"fizz"`
+	Hello string `json:"hello"`
+}
 
 func main() {
-	fmt.Printf("Hello world")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+
+		return c.JSON(HelloWorld{
+			Foo:   "Bar",
+			Fizz:  "Buzz",
+			Hello: "World",
+		})
+	})
+
+	log.Fatal(app.Listen(":3000"))
 }
